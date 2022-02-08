79db4eb643b9c4f0549162a3c25979ff49bab119
Alex Feralix <vaslu85@mail.ru>
2022-02-08 10:03:45 +0200

    created MainPopularCategories component

--- src/components/MainPopularCategories.js
+++ src/components/MainPopularCategories.js
@@ -1,17 +1,36 @@
 import React from "react";
-import Carousel from "nuka-carousel";
 
 import classes from "./MainPopularCategories.module.scss";
 import useHttp from "../hooks/useHttp";
 
 export default function MainPopularCategories() {
-  const types = useHttp(
+  const instrumentTypes = useHttp(
     "https://musical-instruments-c9bcf-default-rtdb.europe-west1.firebasedatabase.app/types.json"
   );
-  console.log(types);
+
+  const displayInstrumentTypes = instrumentTypes.map((type) => (
+    <div className={classes["cat-container"]} key={type.typeName}>
+      <img
+        src={`https://i.ibb.co/${type.typeImgCode}/${type.typeName}.jpg`}
+        alt={type.typeName}
+      />
+      <span>
+        {type.typeName
+          .split("-")
+          .map((word) => {
+            return word.charAt(0).toUpperCase() + word.substring(1);
+          })
+          .join(" ")}
+      </span>
+    </div>
+  ));
+
   return (
-    <div>
-      <p>Check</p>
+    <div className={classes["popular-categories"]}>
+      <h2>Explore Categories</h2>
+      <section className={classes["cat-list"]}>
+        {displayInstrumentTypes}
+      </section>
     </div>
   );
 }
