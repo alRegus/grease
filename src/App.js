083ca773b09c90603734bd045fd63f5c1e149dbf
Alex Feralix <vaslu85@mail.ru>
2022-02-09 07:35:23 +0200

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
-import FrontPage from "./pages/FrontPage";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import FrontPage from "./pages/FrontPage";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
   return (
     <>
       <Header />
-      <FrontPage />
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="category" element={<CategoryPage />} />
+      </Routes>
       <About />
       <Footer />
     </>
---
+++ src/pages/CategoryPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CategoryPage() {
+  return (
+    <div>
+      <p> Hallllo</p>
+    </div>
+  );
+}
