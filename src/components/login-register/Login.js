89494bfee69362f23caef1c67f858794be693077
Alex Feralix <vaslu85@mail.ru>
2022-03-01 18:58:41 +0200

    added styles to Login and Register components

--- src/components/login-register/Login.js
+++ src/components/login-register/Login.js
@@ -1,16 +1,19 @@
 import React from "react";
 
+import classes from "./Login.module.scss";
+
 export default function Login() {
   return (
-    <section>
+    <section className={classes["login-field"]}>
+      <h2>Sign In</h2>
       <form>
         <div>
           <label htmlFor="emailSignIn">Your Email:</label>
-          <input type="email" id="emailSignIn" />
+          <input type="email" id="emailSignIn" placeholder="Email Address" />
         </div>
         <div>
           <label htmlFor="passwordSignIn">Your Password:</label>
-          <input type="password" id="passwordSignIn" />
+          <input type="password" id="passwordSignIn" placeholder="Password" />
         </div>
         <button>SIGN IN</button>
       </form>
--- src/components/login-register/Register.js
+++ src/components/login-register/Register.js
@@ -1,20 +1,27 @@
 import React from "react";
 
+import classes from "./Login.module.scss";
+
 export default function Register() {
   return (
-    <section>
+    <section className={classes["register-field"]}>
+      <h2>Create an Account</h2>
       <form>
         <div>
           <label htmlFor="emailRegister">Your Email:</label>
-          <input type="email" id="emailRegister" />
+          <input type="email" id="emailRegister" placeholder="Email Address" />
         </div>
         <div>
           <label htmlFor="passwordRegister">Your Password:</label>
-          <input type="password" id="passwordRegister" />
+          <input type="password" id="passwordRegister" placeholder="Password" />
         </div>
         <div>
           <label htmlFor="passwordConfirmRegister">Confirm Password:</label>
-          <input type="password" id="passwordConfirmRegister" />
+          <input
+            type="password"
+            id="passwordConfirmRegister"
+            placeholder="Confirm Password"
+          />
         </div>
         <button>CREATE ACCOUNT</button>
       </form>
