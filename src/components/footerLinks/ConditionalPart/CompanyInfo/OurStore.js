3d8e3d133e4a1683471e281e64fd0fdf92e368f5
Alex Feralix <vaslu85@mail.ru>
2022-04-19 19:20:06 +0300

    added styles and logic to OurHistory component

--- src/components/footerLinks/ConditionalPart/CompanyInfo/OurStore.js
+++ src/components/footerLinks/ConditionalPart/CompanyInfo/OurStore.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import CustomerService from "../../CustomerService";
 import HearFromYou from "../../HearFromYou";
@@ -7,7 +8,15 @@ export default function OurStore() {
   return (
     <>
       <CustomerService />
-      <section>Lol</section>
+      {/* <div style={{ width: "300px", height: "300px" }}>
+        <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+      </div> */}
+
       <HearFromYou />
     </>
   );
