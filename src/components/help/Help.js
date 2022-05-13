d8d4b436bc8bdeea6f5a4f79124afc7008f7393c
Alex Feralix <vaslu85@mail.ru>
2022-05-13 06:42:04 +0300

    responsive for Help and HelpPart components + litttle tweak for fonts in CategoryPage

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -12,25 +12,25 @@ export default function Help() {
             icon="fa-solid fa-phone-flip"
             header="Give Us A Call"
             description="Questions? We’re happy to help! Call us at 800-223-2500"
-            linkTo=""
+            linkTo="#"
           />
-          <HelpPart
+          {/* <HelpPart
             icon="fa-solid fa-comment"
             header="Chat Now"
             description="Chat live with an expert and get the help you need now."
             linkTo=""
-          />
+          /> */}
           <HelpPart
             icon="fa-solid fa-envelope"
             header="Email Us"
             description="For questions or help with an order, you can send us an email anytime."
-            linkTo=""
+            linkTo="/help/contactUs"
           />
           <HelpPart
             icon="fa-solid fa-location-dot"
             header="Visit Our Store"
             description="Come visit our New York City store at 42 West 18th Street."
-            linkTo=""
+            linkTo="/info/store"
           />
         </ul>
       </nav>
--- src/components/help/HelpPart.js
+++ src/components/help/HelpPart.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HelpPart({ icon, header, description, linkTo }) {
   return (
     <li>
-      <a href={linkTo}>
+      <Link to={linkTo}>
         <i className={icon}></i>
         <div>
           <h2>{header}</h2>
           <p>{description}</p>
         </div>
-      </a>
+      </Link>
     </li>
   );
 }
